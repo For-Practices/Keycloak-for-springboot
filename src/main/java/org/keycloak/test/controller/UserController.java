5ac422cb108a9e0fffc74f6540c7739334f90999
Yohan Ishara <yohanishara01@gmail.com>
2021-03-29 22:55:12 +0530

    Modified userController with signUp

--- src/main/java/org/keycloak/test/controller/UserController.java
+++ src/main/java/org/keycloak/test/controller/UserController.java
@@ -1,16 +1,33 @@
 package org.keycloak.test.controller;
 
+import org.keycloak.test.model.User;
 import org.keycloak.test.repository.UserRepository;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+import java.util.Optional;
+
 
 @CrossOrigin
 @RestController
-@RequestMapping("/api/v1/")
+@RequestMapping("/api/v1/users/")
 public class UserController {
     @Autowired
     private UserRepository userRepository;
 
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping("/signUp")
+    public User signUp(@RequestBody User user){
+        return userRepository.save(user);
+    }
+
+   /* @GetMapping(value = "/signIn")
+    @ResponseBody
+    public User signIn(@RequestParam Map<String,String> allParams){
+
+    }*/
 }
